481dc8938413ea2ec2043b8c76d75570ad260a93
mmreza <mmreza@pop-os.localdomain>
2023-07-11 18:17:23 +0330

    second

--- code.js
+++ code.js
@@ -1,7 +1,42 @@
 const btn = document.getElementById('submit-btn')
+const food = [];
+const country = [];
+const color = [];
+const firstChar = ''
 
 btn.addEventListener('click', () => {
-    document.getElementById('input-color')
-    document.getElementById('input-country')
-    document.getElementById('input-food')
+    let score = 0;
+    const colorInput = document.getElementById('input-color')
+    const countryInput = document.getElementById('input-country')
+    const foodInput = document.getElementById('input-food')
+
+//     color.push(colorInput);
+//     food.push(foodInput)
+    // country.push(foodInput)
+    if(colorInput[0] === firstChar ) {
+        if(food.includes(foodInput)) {
+            score += 5
+        } else {
+            score += 10
+        }
+    }
+
+    if(colorInput[0] === firstChar) {
+        if(color.includes(colorInput)) {
+            score += 5
+        } else {
+            score += 10
+        }
+    }
+
+    if(countryInput[0] === firstChar) {
+        if(country.includes(countryInput)) {
+            score += 5
+        } else {
+            score += 10
+        }
+    }
+
+
+
 })
