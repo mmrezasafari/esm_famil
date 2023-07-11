8eb33d8c18d05a8549f67218d4fd8db9450d14f9
mmreza <mmreza@pop-os.localdomain>
2023-07-11 18:08:52 +0330

    first commit

---
+++ code.js
@@ -0,0 +1,7 @@
+const btn = document.getElementById('submit-btn')
+
+btn.addEventListener('click', () => {
+    document.getElementById('input-color')
+    document.getElementById('input-country')
+    document.getElementById('input-food')
+})
